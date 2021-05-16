51b5bba3cd19ed650644fdd984a419a99de4a814
AdrianHernandezll <danix08@live.com>
2021-05-16 18:36:53 -0300

    seccion dowload

--- js/main.js
+++ js/main.js
@@ -42,4 +42,27 @@ $(document).ready(function () {
             }
         }
     });
+
+
+    /**-------------------Features Carousel------------------------- */
+    $('.screenshots-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+
+            },
+            600: {
+                items: 2,
+
+            },
+            1000: {
+                items: 4,
+
+            }
+        }
+    });
 });
