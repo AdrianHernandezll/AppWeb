744eab729eb9590999fa13799e0c9f3b31c4983d
AdrianHernandezll <danix08@live.com>
2021-05-15 22:00:41 -0300

    efecto shrink

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-$(document).ready(function (){
+$(document).ready(function () {
+    /**------------Encoger Navbar----------------- */
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 89) {
+            $(".navbar").addClass("navbar-shrink");
+        } else {
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    });
     /**-----------Video popup--------------------- */
     const videoSrc = $('#player-1').attr("src");
-    $(".video-play-btn, .video-popup").on("click", function(){
-        if($(".video-popup").hasClass("open")){
+    $(".video-play-btn, .video-popup").on("click", function () {
+        if ($(".video-popup").hasClass("open")) {
             $(".video-popup").removeClass("open");
-            $('#player-1').attr("src"," ")
-        }else{
+            $('#player-1').attr("src", " ")
+        } else {
             $(".video-popup").addClass("open");
-            if($("#player-1").attr("src")==''){
-                $("#player-1").attr("src",videoSrc);
+            if ($("#player-1").attr("src") == '') {
+                $("#player-1").attr("src", videoSrc);
             }
         }
     });
