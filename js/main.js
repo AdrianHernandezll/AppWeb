2d50016db886f7c7925c0c930016c8a0f798632f
AdrianHernandezll <danix08@live.com>
2021-05-18 19:54:30 -0300

    team section carousel

--- js/main.js
+++ js/main.js
@@ -89,4 +89,27 @@ $(document).ready(function () {
             }
         }
     });
+
+
+    /**-------------------Team Carousel------------------------- */
+    $('.team-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+
+            },
+            600: {
+                items: 2,
+
+            },
+            1000: {
+                items: 3,
+
+            }
+        }
+    });
 });
